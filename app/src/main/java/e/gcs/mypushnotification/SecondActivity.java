923a93d6c086e3672a8ed2f01edc161ebea180e2
VaishnaviKairamkonda <vaishnavikairamkonda@gmail.com>
2021-09-29 16:33:41 +0530

    First Commit

---
+++ app/src/main/java/e/gcs/mypushnotification/SecondActivity.java
@@ -0,0 +1,24 @@
+package e.gcs.mypushnotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView txt = findViewById(R.id.textView);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            for (String key : bundle.keySet()) {
+                Object value = bundle.get(key);
+                txt.append(key + ": " + value + "\n\n");
+            }
+        }
+    }
+}
